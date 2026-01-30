1fedf05b43281523786060060158e40a93bcdcd7
unknown <andremelloti@outlook.com>
2026-01-30 16:15:26 -0300

    Ajustes nas interfaces

--- src/components/typtes/interfaces.ts
+++ src/components/typtes/interfaces.ts
@@ -2,6 +2,7 @@ export  const Contatos = {
     wa:"99999999999",
     cel:"9999999999",
     email:"test@test.com",
+    msgwa:"Olá, vim pelo site. Gostaria de mais informações sobre os serviços oferecidos pela J&P Impressoras.",
     link_wa:"https://wa.me/55999999999",
     link_email:"mailto:test@test.com",
     link_tel:"tel:2199999999999"
