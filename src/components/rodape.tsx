9e26a3b5e5194062b066ef72c8c6aaa561b50b12
unknown <andremelloti@outlook.com>
2026-01-30 20:30:26 -0300

    corrigir path de imagens usar src e import(mais eficiente)

--- src/components/rodape.tsx
+++ src/components/rodape.tsx
@@ -1,16 +1,23 @@
 import '../styles/rodape.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 
 export default function Rodape() {
     return (
         <div className="rodape-container rodape bg-gray-800 ">  
         <div className='flex-box w-full text-start p-4 text-white'>
-            <h5>Nos siga nas redes sociais</h5>
+            <h5 className='text-white inline bg-gradient-to-r from-indigo-600 to-pink-500 p-2'>Nos siga nas redes sociais</h5>
             <div className='grid-cols-5 w-full'>
                 <FontAwesomeIcon icon={faFacebook} className="text-blue-600 hover:text-blue-800 cursor-pointer" />
-
+                <FontAwesomeIcon icon={faInstagram} className="text-blue-600 hover:text-blue-800 cursor-pointer" />
+                <FontAwesomeIcon icon={faXTwitter} className="text-blue-600 hover:text-blue-800 cursor-pointer" />
+                <FontAwesomeIcon icon={faYoutube} className="text-blue-600 hover:text-blue-800 cursor-pointer" />
+                <FontAwesomeIcon icon={faLinkedin} className="text-blue-600 hover:text-blue-800 cursor-pointer" />
             </div>
         
         </div>
